9883b4c00aa22c814721744dcd573a9e1e37b4b0
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:21:14 +0530

    Update solution: Two Sum (python)

--- Array/Two Sum/solution.py
+++ Array/Two Sum/solution.py
@@ -1,9 +1,12 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        storage = {}
-
-        for i , num in enumerate(nums):
-            compilement = target - num
-            if compilement in storage:
-                return [storage[compilement], i]
-            storage[num] = i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return[i, j]
