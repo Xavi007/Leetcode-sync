7696503ad24252f240f7212420a467b1ef8924dd
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:38:55 +0530

    Add solution: Merge Sorted Array (c)

---
+++ Array/Merge Sorted Array/solution.c
@@ -0,0 +1,21 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int i = m-1;  // last element in nums1
+    int j = n-1; // last element in nums2
+    int k = m+n-1; // insert position in nums1
+
+    while((i >= 0) && (j>=0))
+    {
+        if(nums1[i] > nums2[j]) // if nums1 > nums2
+        {
+            nums1[k--] = nums1[i--];
+        }
+        else
+        {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    while(j >= 0) // remaining elements in nums2
+    {
+        nums1[k--] = nums2[j--];
+    }
+}
