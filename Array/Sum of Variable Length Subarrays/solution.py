acb198b26b43c9ac3ed3a69cc2c6cc1872212403
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:19:09 +0530

    Add solution: Sum of Variable Length Subarrays (python3)

---
+++ Array/Sum of Variable Length Subarrays/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        total_sum = 0
+        for i in range(len(nums)):
+            start = max(0, i - nums[i])
+            total_sum += sum(nums[start:i+1])
+        return total_sum
