dbe3e4361c7c2fe4040cdf77f85391c566650f3e
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:05:50 +0530

    Add solution: Single Number II (cpp)

---
+++ Array/Single Number II/solution.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int x : nums) mp[x]++;
+        for (auto &p : mp) {
+            if (p.second == 1) return p.first;
+        }
+        return 0;
+    }
+};
