18917b01f3ea2c7db823333af34791bb657087de
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:09:07 +0530

    Add solution: Jump Game II (python3)

---
+++ Array/Jump Game II/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps, currentend, farthest = 0, 0, 0
+
+
+        for i in range(len(nums)-1):
+            farthest = max(farthest, (i + nums[i]))
+
+            if i == currentend:
+                jumps += 1
+                currentend = farthest
+        return jumps
