a7651cf5135140de58f48cae535b4103a24e006f
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:46:38 +0530

    Delete Bit Manipulation directory

--- Bit Manipulation/Binary Number with Alternating Bits/solution.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    bool hasAlternatingBits(int n) {
-        unsigned int x = (n ^ (n >> 1));
-        return ((x & (x+1u)) == 0);
-    }
-};
--- Bit Manipulation/Number Complement/solution.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findComplement(self, num: int) -> int:
-        mask = 1
-        while mask <= num:
-            mask <<= 1
-        return (mask - 1) ^ num
