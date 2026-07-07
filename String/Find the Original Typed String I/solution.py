c89974911b8ca5514c12c9224b6913cc34b5f180
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:44:40 +0530

    Add solution: Find the Original Typed String I (python3)

---
+++ String/Find the Original Typed String I/solution.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        count = 1  
+        for i in range(1, len(word)):
+            if word[i] == word[i - 1]:
+                count += 1 
+        return count
+
