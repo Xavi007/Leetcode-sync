6f5cbb34d25313345759c06edc4c809d07e27f2d
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:17:03 +0530

    Add solution: Length of Longest Fibonacci Subsequence (python3)

---
+++ Array/Length of Longest Fibonacci Subsequence/solution.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        index = {x: i for i, x in enumerate(arr)}  
+        dp = defaultdict(lambda: 2)  
+        max_length = 0  
+
+        for k in range(len(arr)):
+            for j in range(k):
+                i = index.get(arr[k] - arr[j])
+                if i is not None and i < j:  
+                    dp[j, k] = dp[i, j] + 1  
+                    max_length = max(max_length, dp[j, k])  
+
+        return max_length if max_length >= 3 else 0  
+
