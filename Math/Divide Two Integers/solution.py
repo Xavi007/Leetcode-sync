e540a4c03a6a7e19991408fb3f6af51b01ee1940
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:47:26 +0530

    Delete Math directory

--- Math/Divide Two Integers/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def divide(self, a: int, b: int) -> int:
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
-        
-        if ((a == INT_MIN) and (b == -1)):
-            return 2**31 - 1
-        else:
-            return int(a/b) 
--- Math/Prime Number of Set Bits in Binary Representation/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countPrimeSetBits(self, left: int, right: int) -> int:
-        ans = 0
-        for i in range(left, right+1):
-            count_bits = i.bit_count()
-
-            if (count_bits > 1 and all(count_bits % i for i in range(2, int(count_bits**0.5) + 1))):
-                ans += 1
-        return ans
