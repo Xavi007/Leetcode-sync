8793b7fbb4509e0525d571a140b689ad5be759df
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:00:23 +0530

    Add solution: Sum Root to Leaf Numbers (python3)

---
+++ Medium/Sum Root to Leaf Numbers/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, node, path):
+        if not node:
+            return 0
+        path = 10 * path + node.val
+        if not node.left and not node.right:
+            return path
+        return self.dfs(node.left, path) + self.dfs(node.right, path)
+
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        return self.dfs(root, 0)
