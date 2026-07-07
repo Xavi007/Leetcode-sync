a2957e55047775e1c5456894ae5e0e01e5d2fe9c
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:41:32 +0530

    Add solution: 4Sum (python3)

---
+++ Array/4Sum/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        quadruplets = []
+
+        for a in range(len(nums) - 3):
+            if a > 0 and nums[a] == nums[a-1]:
+                continue
+            
+            for b in range(a + 1, len(nums) - 2):
+                if b > a + 1 and nums[b] == nums[b-1]:
+                    continue
+                
+                c, d = b + 1, len(nums) - 1
+
+                while c < d:
+                    current_sum = nums[a] + nums[b] + nums[c] + nums[d]
+                    
+                    if current_sum == target:
+                        quadruplets.append([nums[a], nums[b], nums[c], nums[d]])
+                        c += 1
+                        d -= 1
+
+                        while c < d and nums[c] == nums[c - 1]:
+                            c += 1
+                        while c < d and nums[d] == nums[d + 1]:
+                            d -= 1
+                    elif current_sum < target:
+                        c += 1
+                    else:
+                        d -= 1
+        return quadruplets
