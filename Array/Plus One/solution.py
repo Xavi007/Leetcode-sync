02f5ec1f4216fd56df4036840768bc9d50197328
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:06:07 +0530

    Add solution: Plus One (python3)

---
+++ Array/Plus One/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        num = 0
+
+        for i in range(len(digits)):
+            num = num * 10 + digits[i]
+        num += 1
+        return [int(d) for d in str(num)]
