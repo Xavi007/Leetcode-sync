b281ad1f413b4cf99ddf4faed3d6b0ba05ad89f4
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:13:06 +0530

    Add solution: Find Words Containing Character (python3)

---
+++ Array/Find Words Containing Character/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        i = 0
+        for word in words:
+            if x in word:
+                ans.append(i)
+            i+=1
+        return ans
