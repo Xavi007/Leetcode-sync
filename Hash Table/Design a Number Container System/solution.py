5139075a95b8d8c035e32c49df316bbd8608eaeb
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 14:18:12 +0530

    Add solution: Design a Number Container System (python3)

---
+++ Hash Table/Design a Number Container System/solution.py
@@ -0,0 +1,21 @@
+class NumberContainers:
+    def __init__(self):
+        self.index_val = {}  
+        self.res = {}  
+    def change(self, index: int, number: int) -> None:
+        if index in self.index_val and (self.index_val[index] == number):
+            return
+        self.index_val[index] = number
+        
+        if number not in self.res:
+            self.res[number] = []
+        heapq.heappush(self.res[number], index)
+
+    def find(self, number: int) -> int:
+        if number not in self.res:
+            return -1
+        
+        while self.res[number] and self.index_val[self.res[number][0]] != number:
+            heapq.heappop(self.res[number])
+
+        return self.res[number][0] if self.res[number] else -1
