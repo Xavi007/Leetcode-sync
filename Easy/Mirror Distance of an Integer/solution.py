2ad7a2f4e2e40df1a5b4d9817d5a4d6251d24f28
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-07 13:09:32 +0530

    Delete Easy directory

--- Easy/Mirror Distance of an Integer/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def mirrorDistance(self, n: int) -> int:
-        copyn = n
-        revn = 0  
-        while (n):
-            digit = n % 10
-            revn = revn * 10 + digit
-            n //= 10
-        return abs(revn - copyn)
