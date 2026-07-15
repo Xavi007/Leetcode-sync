f8a6a4e9b1e6587da92730d7295cfd97a39b1a25
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-16 01:01:31 +0530

    Update solution: Climbing Stairs (python3)

--- Math/Climbing Stairs/solution.py
+++ Math/Climbing Stairs/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n == 1:
+        if ((n == 0) or (n == 1)):
             return 1
-
+            
         dp = [0] * (n + 1)
         dp[0] = dp[1] = 1
 
