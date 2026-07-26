dbf879f6d55e33b097edc5ebd0f1e6a7327a1281
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-26 12:55:57 +0530

    Update solution: Min Cost Climbing Stairs (cpp)

--- Array/Min Cost Climbing Stairs/solution.cpp
+++ Array/Min Cost Climbing Stairs/solution.cpp
@@ -3,16 +3,17 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
 
-        vector<int>dp(n+1);
-
-        dp[0] = 0;
-        dp[1] = 0;
+        int prev2 = 0;
+        int prev1 = 0;
+        int result = 0;
 
         for(int i = 2; i <= n; ++i)
         {
-            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+            result = min(prev1 + cost[i-1], prev2 + cost[i-2]);
+            prev2 = prev1;
+            prev1 = result;
         }
 
-        return dp[n];
+        return result;
     }
 };
