bd9fef49406da1bb3bc9ac655455e4ac6f26cf3c
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2026-07-30 22:24:59 +0530

    Update solution: House Robber (cpp)

--- Array/House Robber/solution.cpp
+++ Array/House Robber/solution.cpp
@@ -5,20 +5,17 @@ public:
 
         if (n == 1)
             return nums[0];
-        if (n == 2)
-            return max(nums[0], nums[1]);
 
-        int prev1 = nums[0];
-        int prev2 = max(nums[0], nums[1]);
-        int result = prev2;
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
         for (int i = 2; i < n; ++i) 
         {
-            result = max(prev2, nums[i] + prev1);
-            prev1 = prev2;
-            prev2 = result;
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
         }
 
-        return result;
+        return dp[n - 1];
     }
 };
